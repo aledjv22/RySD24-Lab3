5b771f7a90914567232414851ea66a04f17bfc16
Victor Alejandro Díaz Jáuregui <victor.diaz.jauregui@mi.unc.edu.ar>
2024-05-15 10:39:32 -0300

    Modificaciones en TransportRx

--- TransportRx.cc
+++ TransportRx.cc
@@ -8,17 +8,15 @@ using namespace omnetpp;
 
 class TransportRx: public cSimpleModule {
     private:
-        cQueue bufferPackets; 
-        cQueue feedbackQueue; 
-        cMessage *serviceEndEvent; 
-        cMessage *feedbackEndEvent; 
-        cOutVector packetBufferSizeVec; 
-        cOutVector packetDropVec; 
-        int packetDropCounter; 
+        cOutVector bufferSizeQueue;
+        cOutVector packetDropQueue;
+        cQueue bufferPackets;
+        cQueue feedbackQueue;
+        cMessage *serviceEndEvent;
+        cMessage *feedbackEndEvent;
+        simtime_t serviceTime;
+        bool feedbackSent;
 
-        void transmitPacket(); 
-        void transmitFeedback(); 
-        void addFeedbackToQueue(cMessage *msg); 
     public:
         TransportRx();
         virtual ~TransportRx();
@@ -43,80 +41,69 @@ TransportRx::~TransportRx() {
 
 void TransportRx::initialize() {
     bufferPackets.setName("Buffer del receptor");
-    feedbackQueue.setName("Buffer de retroalimentación");    
-    packetBufferSizeVec.setName("Tamaño del buffer");
-    packetDropVec.setName("Paquetes descartados");
-    packetDropCounter = 0;
+    bufferSizeQueue.setName("Tamaño del buffer");
+    packetDropQueue.setName("Paquetes descartados");
+    feedbackQueue.setName("Buffer de retroalimentación");
+    packetDropQueue.record(0);
     serviceEndEvent = new cMessage("Fin Servicio");
     feedbackEndEvent = new cMessage("Fin Retroalimentación");
+    feedbackSent = false;
 }
 
 void TransportRx::finish() {
-    recordScalar("Paquetes decartados", packetDropCounter);
-}
-
-void TransportRx::transmitPacket() {
-    if (!bufferPackets.isEmpty()) {
-        // si hay paquetes en el buffer, envía el siguiente
-        cPacket *pkt = (cPacket*) bufferPackets.pop();
-        send(pkt, "toApp");
-        scheduleAt(simTime() + pkt->getDuration(), serviceEndEvent);
-    }
-}
-
-void TransportRx::addFeedbackToQueue(cMessage *msg){
-    if (feedbackQueue.getLength() < par("Tamaño del búfer de retroalimentación").intValue()) {
-        feedbackQueue.insert(msg);
-        if (!feedbackEndEvent->isScheduled()) {
-            scheduleAt(simTime() + 0, feedbackEndEvent);
-        }
-    } else {
-        delete msg;
-    }
-}
-
-void TransportRx::transmitFeedback() {
-    if (!feedbackQueue.isEmpty()) {
-        // si el buffer de retroalimentación no está vacío, envía el siguiente
-        FeedbackPkt *pkt = (FeedbackPkt*) feedbackQueue.pop();
-        send(pkt, "toOut$o");
-        scheduleAt(simTime() + pkt->getDuration(), feedbackEndEvent);
-    }
+    recordScalar("Paquetes_decartados", packetDropQueue.getCount());
 }
 
 void TransportRx::handleMessage(cMessage *msg) {
-    packetBufferSizeVec.record(bufferPackets.getLength());
+    bufferSizeQueue.record(bufferPackets.getLength());
     if (msg == serviceEndEvent) {
-        // el mensaje es serviceEndEvent
-        transmitPacket();    
+        if (!bufferPackets.isEmpty()) {
+            cPacket *pkt = (cPacket*) bufferPackets.pop();
+            send(pkt, "toApp");
+            serviceTime = pkt->getDuration();
+            scheduleAt(simTime() + serviceTime, serviceEndEvent);
+        }
     } else if (msg == feedbackEndEvent) {
-        // el mensaje es feedbackEndEvent
-        transmitFeedback();    
+        if (!feedbackQueue.isEmpty()) {
+            cPacket *pkt = (cPacket*) feedbackQueue.pop();
+            send(pkt, "toOut$o");
+            scheduleAt(simTime() + pkt->getDuration(), feedbackEndEvent);
+        }
     } else {
-        // encola el mensaje
-        if (msg->getKind() == 2) {
-            const int threshold = 0.70 * par("Tamaño Buffer").intValue();
-            if (bufferPackets.getLength() < par("Tamaño Buffer").intValue()) {
-                if (bufferPackets.getLength() >= threshold){
-                    FeedbackPkt *fPkt = new FeedbackPkt();
-                    fPkt->setkind(2);
-                    fPkt->setByteLength(1);
-                    addFeedbackToQueue(fPkt);                
+        if (bufferPackets.getLength() >= par("Tamaño_Buffer").intValue()) {
+            delete(msg);
+            this->bubble("Paquete_descartado");
+            packetDropQueue.record(1);
+        } else {
+            if (msg->getKind() == 2 || msg->getKind() == 3){
+                feedbackQueue.insert(msg);
+                if (!feedbackEndEvent->isScheduled()) {
+                    scheduleAt(simTime() + 0, feedbackEndEvent);
+                }
+            } else {
+                float umbral = 0.80 * par("Tamaño_Buffer").intValue();
+                float umbralMin = 0.25 * par("Tamaño_Buffer").intValue();
+                if (bufferPackets.getLength() >= umbral && !feedbackSent){
+                    cPacket *feedbackPkt = new cPacket("Paquete");
+                    feedbackPkt->setByteLength(20);
+                    feedbackPkt->setKind(2);
+                    send(feedbackPkt, "toOut$o");
+                    feedbackSent = true;
+                }else if (bufferPackets.getLength() < umbralMin && feedbackSent){
+                    cPacket *feedbackPkt = new cPacket("Paquete");
+                    feedbackPkt->setByteLength(20);
+                    feedbackPkt->setKind(3);
+                    send(feedbackPkt, "toOut$o");
+                    feedbackSent = false;
                 }
                 bufferPackets.insert(msg);
+                bufferSizeQueue.record(bufferPackets.getLength());
                 if (!serviceEndEvent->isScheduled()) {
                     scheduleAt(simTime() + 0, serviceEndEvent);
                 }
-            } else {
-                this->bubble("Paquete descartado");
-                packetDropCounter++;
-                packetDropVec.record(packetDropCounter);
-                delete msg;
             }
-        } else{
-            // el mensaje es un paquete de retroalimentación
-            addFeedbackToQueue(msg);
         }
     }
 }
+
 #endif
